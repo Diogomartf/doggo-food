1dd62cac7f79b591e168bddd3cfca6b53d2ea329
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2019-08-04 18:16:58 +0100

    Add inter ui font

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles';
+import './styles/global.css';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
